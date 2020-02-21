a88d141ca751080413118cd3b2fc94efb380b148
Marius Zienius <marius.zienius@gmail.com>
2020-02-21 09:56:57 +0100

    URL sending to backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Result from './components/Result';
+import UrlForm from './components/UrlForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <UrlForm />
       <Result />
     </div>
   );
---
+++ src/components/UrlForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './UrlForm.css';
+
+class UrlForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { url: "" };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        var urlVal = this.state.url;
+        console.log(urlVal);
+        axios.post('/api/parser', urlVal)
+            .then(res => console.log(res.data)
+            .catch(err => console.log('ERROR: can\'t post URL'))
+            );
+        this.setState({ url: "" });
+    }
+    render() { 
+        return (
+            <form className="UrlLinkForm" onSubmit={this.handleSubmit}>
+                <label htmlFor='url'>Your's URL link:</label>
+                <input
+                    type='text'
+                    placeholder='http://...'
+                    id='url'
+                    name='url'
+                    value={this.state.url}
+                    onChange={this.handleChange}
+                />
+                <button>Parse!</button>
+            </form>
+        );
+    }
+}
+ 
+export default UrlForm;
